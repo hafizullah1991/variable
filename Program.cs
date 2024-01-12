35f4b9b5dac2379f9476e1478c1f2acab11bf228
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-11 18:31:31 -0800

    variable exercise1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string position = "Software Engineer";
+
+            int num = 2;
+            char B= 'A';
+            bool isTrue = true;
+            double year = 0.5;
+            decimal netWorth = 1.5m;
+            Console.WriteLine($"My biggest dream is to be {position}," +
+                $" in next {num} months , in next {year} year I want to " +
+                $"work for Google \n, and my net worth well be {netWorth} million dollars\n" +
+                $" , it's all {isTrue} also I want to be {B} plus" );
+        }
+    }
+}
